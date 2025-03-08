5ed9a1679cc833c98a9218a9605d74e9090c5ec2
minhnguyennhat <144748251+MinhNguyenNhat0612@users.noreply.github.com>
2025-03-08 10:53:37 +0700

    [Fixing] Payment

--- Services/ApiModels/Payment/PaymentRequest.cs
+++ Services/ApiModels/Payment/PaymentRequest.cs
@@ -4,15 +4,15 @@ namespace Services.ApiModels.Payment
 {
     public class PaymentRequest
     {
-        public string OrderId { get; set; }
+        public string OrderCode { get; set; }
         public decimal Amount { get; set; }
         public string Description { get; set; }
-        public PaymentTypeEnums PaymentType { get; set; }
-        public int ServiceId { get; set; } // ID của BookingOnline/BookingOffline/Course/Workshop
+        public string CancelUrl { get; set; }
+        public string ReturnUrl { get; set; }
         public string CustomerName { get; set; }
         public string CustomerEmail { get; set; }
         public string CustomerPhone { get; set; }
-        public string ReturnUrl { get; set; }
-        public string CancelUrl { get; set; }
+        public PaymentTypeEnums PaymentType { get; set; }
+        public string ServiceId { get; set; }
     }
 } 
--- Services/Interfaces/IPaymentService.cs
+++ Services/Interfaces/IPaymentService.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessObjects;
+using BusinessObjects.Models;
 using Services.ApiModels.Payment;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,15 @@ namespace Services.Interfaces
 {
     public interface IPaymentService
     {
+        PaymentRequest CreateBookingOnlinePaymentRequest(BookingOnline bookingOnline);
+        PaymentRequest CreateBookingOfflinePaymentRequest(BookingOffline bookingOffline);
+        PaymentRequest CreateCoursePaymentRequest(Course course, Customer customer = null);
+        PaymentRequest CreateWorkshopPaymentRequest(WorkShop workshop, Customer customer = null);
         Task<PaymentResponse> CreatePaymentAsync(PaymentRequest request);
         Task<PaymentResponse> CheckPaymentStatusAsync(string orderId);
+        
+        // Thêm phương thức mới để tự động điền thông tin khách hàng
+        Task<PaymentRequest> PopulateCustomerInfoForPaymentRequest(PaymentRequest request);
+        Task<PaymentResponse> ProcessPayment(PaymentRequest request);
     }
 }

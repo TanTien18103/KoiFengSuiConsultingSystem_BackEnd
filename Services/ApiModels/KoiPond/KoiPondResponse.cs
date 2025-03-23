2f480b43fff509539ba14b9f7e6a2e727725efd6
minhnguyennhat <144748251+MinhNguyenNhat0612@users.noreply.github.com>
2025-03-24 00:39:07 +0700

    [Fixed] Koipond Response

--- Services/ApiModels/KoiPond/KoiPondResponse.cs
+++ Services/ApiModels/KoiPond/KoiPondResponse.cs
@@ -7,7 +7,8 @@
         public string ShapeId { get; set; }
         public string ShapeName { get; set; }
         public string ImageUrl { get; set; }
-        public string Direction { get; set; }
         public string Element { get; set; }
+        public string Introduction { get; set; }
+        public string Description { get; set; }
     }
 }

9d8c2dc5b707ed8fe4010fd7539805d6ff302b27
Tan Tien <tiennhtse173499@fpt.edu.vn>
2025-04-13 19:11:38 +0700

    UPDATE - Logic for GetKoiVarietiesByElementAsync

--- DAOs/DAOs/KoiVarietyDAO.cs
+++ DAOs/DAOs/KoiVarietyDAO.cs
@@ -78,7 +78,7 @@ namespace DAOs.DAOs
             return await _context.KoiVarieties
                 .Include(k => k.VarietyColors)
                     .ThenInclude(vc => vc.Color)
-                .Where(k => k.VarietyColors.Any(vc => vc.Color.Element == element && vc.Percentage > 60))
+                .Where(k => k.VarietyColors.Any(vc => vc.Color.Element == element))
                 .ToListAsync();
         }
 
@@ -121,13 +121,13 @@ namespace DAOs.DAOs
         public async Task DeleteKoiVarietyDao(string koiVarietyId)
         {
             var koiVariety = await _context.KoiVarieties
-                .Include(kv => kv.VarietyColors) 
+                .Include(kv => kv.VarietyColors)
                 .FirstOrDefaultAsync(kv => kv.KoiVarietyId == koiVarietyId);
 
             if (koiVariety != null)
             {
                 _context.VarietyColors.RemoveRange(koiVariety.VarietyColors);
-                await _context.SaveChangesAsync(); 
+                await _context.SaveChangesAsync();
 
                 _context.KoiVarieties.Remove(koiVariety);
                 await _context.SaveChangesAsync();
